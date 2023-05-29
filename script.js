994d6a24538097402e8cf7a0553ed24429de7385
ShriGanesh Saini <67692684+rvsaini8107@users.noreply.github.com>
2023-05-29 12:45:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function threeSum(arr, target) {
 // write your code here
 	console.log(arr, target,"hhhh")
-  
+	document.write(arr, target,"hhhh")
+  return(arr, target,"hhhh")
 }
 
 module.exports = threeSum;
